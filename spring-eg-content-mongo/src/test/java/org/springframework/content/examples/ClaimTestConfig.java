0dae05a472c20fba6537a94544029913d0ab5f75
Paul Warren <paul.warren@emc.com>
2016-02-19 21:48:15 -0800

    Mongo 3 connection

--- spring-eg-content-mongo/src/test/java/org/springframework/content/examples/ClaimTestConfig.java
+++ spring-eg-content-mongo/src/test/java/org/springframework/content/examples/ClaimTestConfig.java
@@ -1,19 +1,24 @@
 package org.springframework.content.examples;
 
 
+import java.util.Arrays;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.content.config.EnableMongoContentStores;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 @ComponentScan
@@ -30,35 +35,36 @@ public class ClaimTestConfig extends AbstractMongoConfiguration {
 	
 	@Override
 	protected String getDatabaseName() {
-		return "spring-eg-content-mongo";
+		return "spring-content";
+	}
+
+	@Override
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		
+		if (System.getenv("spring_eg_content_mongo_host") != null) {
+	    	String host = System.getenv("spring_eg_content_mongo_host");
+	    	String port = System.getenv("spring_eg_content_mongo_port");
+	    	String username = System.getenv("spring_eg_content_mongo_username");
+	    	String password = System.getenv("spring_eg_content_mongo_password");
+
+	    	log.info(String.format("Connecting to %s:%s", host, port));
+
+			 // Set credentials      
+		    MongoCredential credential = MongoCredential.createCredential(username, getDatabaseName(), password.toCharArray());
+		    ServerAddress serverAddress = new ServerAddress(host, Integer.parseInt(port));
+	
+		    // Mongo Client
+		    MongoClient mongoClient = new MongoClient(serverAddress,Arrays.asList(credential)); 
+	
+		    // Mongo DB Factory
+		    return new SimpleMongoDbFactory(mongoClient, getDatabaseName());
+		}
+		return super.mongoDbFactory();
 	}
 
 	@Override
 	public Mongo mongo() throws Exception {
-		if (System.getenv("spring_eg_content_mongo_host") != null) {
-	    	String host = System.getenv("spring_eg_content_mongo_host");
-	    	String port = System.getenv("spring_eg_content_mongo_port");
-
-	    	log.info(String.format("Connecting to %s:%s", host, port));
-	        int nPort = Integer.parseInt(port);
-	        Mongo mongo = new MongoClient(host, nPort);
-	        return mongo;
-	    } else {
-	    	log.info("Connecting to localhost");
-	        return new MongoClient();
-	    }	
+    	log.info("Connecting to localhost");
+        return new MongoClient();
 	}
-	
-    @Override
-	protected UserCredentials getUserCredentials() {
-
-    	if (System.getenv("spring_eg_content_mongo_username") != null) {
-	        String username = System.getenv("spring_eg_content_mongo_username");
-	        String password = System.getenv("spring_eg_content_mongo_password");
-	    	log.info(String.format("Connecting as %s:%s", username, password));
-		    return new UserCredentials(username, password);
-	    }
-    	return null;
-	}
-
 }
