e522c863b8b54acb45beda13f26717d391c532e5
Paul Warren <paul_c_warren@yahoo.com>
2016-06-16 07:54:13 -0700

    Ginkgo4j, the sequel

--- spring-eg-starter-content-jpa/src/test/java/org/springframework/content/examples/ClaimTest.java
+++ spring-eg-starter-content-jpa/src/test/java/org/springframework/content/examples/ClaimTest.java
@@ -4,7 +4,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+
+@RunWith(Ginkgo4jSpringRunner.class)
+@Ginkgo4jConfiguration(threads=1)
 @SpringApplicationConfiguration(classes = org.springframework.content.examples.Application.class)   
 public class ClaimTest extends AbstractSpringContentTests {
 
--- spring-eg-starter-content-mongo/src/test/java/org/springframework/content/examples/ClaimTest.java
+++ spring-eg-starter-content-mongo/src/test/java/org/springframework/content/examples/ClaimTest.java
@@ -2,9 +2,12 @@ package org.springframework.content.examples;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+
+@RunWith(Ginkgo4jSpringRunner.class)
+@Ginkgo4jConfiguration(threads=1)
 @SpringApplicationConfiguration(classes = org.springframework.content.examples.Application.class)   
 public class ClaimTest extends AbstractSpringContentTests {
 
