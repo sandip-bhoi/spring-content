9805949fa460cd309ef8893ffc55f06984186843
Paul Warren <paul_c_warren@yahoo.com>
2016-06-15 20:52:08 -0700

    Revert "Move over to ginkgo4j test framework"
    This reverts commit 83fb519aaf79063a15afd892d322a987d70ae492.

--- spring-eg-content-s3/src/test/java/org/springframework/content/examples/ClaimTest.java
+++ spring-eg-content-s3/src/test/java/org/springframework/content/examples/ClaimTest.java
@@ -4,11 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
-
-@RunWith(Ginkgo4jSpringRunner.class)
-@Ginkgo4jConfiguration(threads=1)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { ClaimTestConfig.class })
 public class ClaimTest extends AbstractSpringContentTests {
 
--- spring-eg-starter-content-jpa/src/test/java/org/springframework/content/examples/ClaimTest.java
+++ spring-eg-starter-content-jpa/src/test/java/org/springframework/content/examples/ClaimTest.java
@@ -2,12 +2,10 @@ package org.springframework.content.examples;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
-
-@RunWith(Ginkgo4jSpringRunner.class)
-@Ginkgo4jConfiguration(threads=1)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = org.springframework.content.examples.Application.class)   
 public class ClaimTest extends AbstractSpringContentTests {
+
 }
